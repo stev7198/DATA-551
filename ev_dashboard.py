af41df583f1ed9ab4ce75be07b2b3a7b910737ba
Aamna <aamna-amer>
2025-02-27 11:47:09 -0800

    added slider

--- ev_dashboard.py
+++ ev_dashboard.py
@@ -13,37 +13,59 @@ app = Dash()
 app.layout = html.Div([
     html.H1("EV Chargers Around the World", style={'textAlign': 'center'}),
 
+    #container for everything
     html.Div([
 
-#dropdown menu for charger type
-    dcc.Dropdown(
-        id='charger-type-dropdown',
-        options=[
-            {'label': 'AC Level 1', 'value': 'AC Level 1'},
-            {'label': 'AC Level 2', 'value': 'AC Level 2'},
-            {'label': 'DC Fast Charger', 'value': 'DC Fast Charger'},
-            {'label': 'Show All Chargers', 'value': 'All'}
-        ],
-        value='All',
-        style={'width': '48%', 'display': 'inline-block'}
-    ),
+        #container for dropdown and slider
+        html.Div([
 
-    dcc.Graph(
-        id='map',
-        config={'scrollZoom': True}
-    )
-],
-style={'display': 'flex', 'flexDirection': 'row', 'alignItems': 'center'}),
-])
+            #dropdown for charger type
+            dcc.Dropdown(
+                id='charger-type-dropdown',
+                options=[
+                    {'label': 'AC Level 1', 'value': 'AC Level 1'},
+                    {'label': 'AC Level 2', 'value': 'AC Level 2'},
+                    {'label': 'DC Fast Charger', 'value': 'DC Fast Charger'},
+                    {'label': 'Show All Chargers', 'value': 'All'}
+                ],
+                value='All',
+                style={'width': '100%', 'display': 'inline-block'}
+            ), 
+
+            #slider based on installation year
+            dcc.Slider(
+                id='crossfilter-year-slider',
+                min=ev["Installation Year"].min(),
+                max=ev["Installation Year"].max(),
+                step=1,
+                value=ev['Installation Year'].max(),
+                marks={str(year): str(year) for year in sorted(ev["Installation Year"].unique())},
+                tooltip={"placement": "bottom", "style": {"width": '50%'}, "always_visible": True}
+            ),
+        ], style={'width': '40%', 'display': 'flex', 'flexDirection': 'column', 'alignItems': 'left'}),
+
+
+
+        #container for graph
+        html.Div([
+            dcc.Graph(
+                id='map',
+                config={'scrollZoom': True}
+            ),
+        ], style={'width': '60%'}),
+    ], style={'display': 'flex', 'flexDirection': 'row', 'alignItems': 'flex-start', 'width': '100%'}),
+], style={'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center', 'width': '100%'}
+)
 
 #callback to update map
 @app.callback(
     Output('map', 'figure'),
     Input('charger-type-dropdown', 'value'),
+    Input('crossfilter-year-slider', 'value'),
     [Input('map', 'relayoutData')]
 )
 
-def update_map(charger_type, relayoutData):
+def update_map(charger_type, year_value, relayoutData):
 
 #default values for zoom and positioning of map
     zoom = 1
@@ -57,7 +79,10 @@ def update_map(charger_type, relayoutData):
 
     fig = go.Figure()
 
-#filter data type based on dropdown selection
+    #filter data by installation year based on slider selection
+    filtered_ev = filtered_ev[filtered_ev["Installation Year"] == year_value]
+    
+    #filter data by charger type based on dropdown selection
     if charger_type != 'All':
         filtered_ev = filtered_ev[filtered_ev['Charger Type'] == charger_type]
 
